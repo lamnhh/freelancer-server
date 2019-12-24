cdab6c24c326ec83a6d4b11f0d172a9eab671557
Lam Nguyen <lam.nguyen.hy.hoai@gmail.com>
2019-12-24 10:01:01 +0700

    Added connecting using connection_string

--- configs/db.js
+++ configs/db.js
@@ -1,12 +1,20 @@
 let { Pool } = require("pg");
 require("dotenv").config();
 
-let pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT
-});
+// Connect using connection string if available.
+// Otherwise, connect using credentials.
+let config = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL
+    }
+  : {
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASS,
+      port: process.env.DB_PORT
+    };
+
+let pool = new Pool(config);
 
 module.exports = pool;
