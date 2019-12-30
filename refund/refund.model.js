d6195e4205d59dc3c14c36ce7186b662789f43f2
Lam Nguyen <lam.nguyen.hy.hoai@gmail.com>
2019-12-30 13:39:52 +0700

    Added notification after refund request creation

--- refund/refund.model.js
+++ refund/refund.model.js
@@ -1,6 +1,7 @@
 let db = require("../configs/db");
 let { normaliseString } = require("../configs/types");
 let moment = require("moment");
+let { createNotification } = require("../notification/noti.model");
 
 /**
  * Create a refund request.
@@ -9,18 +10,26 @@ let moment = require("moment");
  * @param {String} reason
  */
 async function createRefundRequest(username, transactionId, reason) {
-  let transaction = await db
-    .query(`SELECT * FROM transactions WHERE id=$1`, [transactionId])
-    .then(function({ rows }) {
-      if (rows.length !== 1) {
-        throw {
-          http: 404,
-          code: "NO_TRANSACTION",
-          message: `No transaction with ID '${transactionId}' exists`
-        };
-      }
-      return normaliseString(rows[0]);
-    });
+  let sql = `
+  SELECT 
+    transactions.*,
+    jobs.name as job_name,
+    jobs.username as seller
+  FROM
+    transactions
+    JOIN jobs ON (transactions.job_id = jobs.id)
+  WHERE
+    transactions.id = $1;`;
+  let transaction = await db.query(sql, [transactionId]).then(function({ rows }) {
+    if (rows.length !== 1) {
+      throw {
+        http: 404,
+        code: "NO_TRANSACTION",
+        message: `No transaction with ID '${transactionId}' exists`
+      };
+    }
+    return normaliseString(rows[0]);
+  });
 
   // A user can only ask for refund of their own transactions.
   if (transaction.username !== username) {
@@ -41,7 +50,7 @@ async function createRefundRequest(username, transactionId, reason) {
   }
 
   // Request for refund can only be made within 3 days after transaction is finished.
-  if (moment(transaction.finished_at).diff(moment.now()) <= 86400000 * 3) {
+  if (moment().diff(transaction.finished_at) > 86400000 * 3) {
     throw {
       http: 405,
       code: "NOT_ALLOWED",
@@ -49,7 +58,19 @@ async function createRefundRequest(username, transactionId, reason) {
     };
   }
 
-  let sql = `INSERT INTO refund_requests(transaction_id, reason) VALUES ($1, $2) RETURNING *`;
+  // Create notification to buyer
+  await createNotification(
+    transaction.username,
+    `We have received your refund request for '${transaction.job_name}'`
+  );
+
+  // Create notification to seller
+  await createNotification(
+    transaction.seller,
+    `User '${transaction.username}' has filed a refund request for your '${transaction.job_name}'`
+  );
+
+  sql = `INSERT INTO refund_requests(transaction_id, reason) VALUES ($1, $2) RETURNING *`;
   return await db.query(sql, [transactionId, reason]).then(function({ rows }) {
     return rows[0];
   });
@@ -111,7 +132,7 @@ function findAllRequests() {
   WHERE
     refund_requests.status IS NULL`;
   return db.query(sql).then(function({ rows }) {
-    return rows;
+    return rows.map(normaliseString);
   });
 }
 
